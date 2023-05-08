0f1e8f8574565bdccb3d75988d58c8295575239e
Tursunboy <123323706+Tursunboy-005@users.noreply.github.com>
2023-05-08 11:53:55 +0500

    klj

--- Collection/Collection/Program.cs
+++ Collection/Collection/Program.cs
@@ -10,6 +10,30 @@ namespace Collection
     {
         static void Main(string[] args)
         {
+            Person<int, string> person = new Person<int, string>();
+            
+            
         }
     }
 }
+
+class Person <T,K>
+{
+    public T Id { get; set; }
+    public string Name { get; set; }
+
+    //public T Calculate(KeyNotFoundException param)
+    //{
+    //    return Id;
+    //}
+    public bool IsEquel(K a,K b)
+    {
+        return a.Equals(b);
+    }
+}
+
+class MyIdClass
+{
+
+}
+
